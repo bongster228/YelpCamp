83d8e2a78e0740aa3e4c581da1bcd8d772335c86
Bong Lee <bongster228@github.com>
2020-02-24 20:08:26 -0800

    BootStrap added

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('landing');
+});
+
+app.get('/campgrounds', (req, res) => {
+  const campgrounds = [
+    { name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80' },
+    { name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' },
+    { name: 'Mountain Goat Rest', image: 'https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' },
+  ];
+
+  res.render('campgrounds', { campgrounds });
+});
+
+app.listen(PORT, () => {
+  console.log('YelpCamp server has started');
+});
