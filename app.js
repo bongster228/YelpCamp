bc925c9eda31ce32749fc0810cbfe3b801aee69a
Bong Lee <bongster228@github.com>
2020-03-09 23:53:23 -0700

    v3

--- app.js
+++ app.js
@@ -24,22 +24,6 @@ const campgroundSchema = new mongoose.Schema({
 
 const Campground = mongoose.model('Campground', campgroundSchema);
 
-// Campground.create(
-//   {
-//     name: 'Granite Hill',
-//     image:
-//       'https://images.unsplash.com/photo-1576949223184-431f5c6eb29b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-//     description: 'Awesome place to camp'
-//   },
-//   (err, campground) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log(campground);
-//     }
-//   }
-// );
-
 //---------------------------------------------------------------------------------------------
 // Routes
 app.get('/', (req, res) => {
